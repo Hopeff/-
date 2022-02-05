f480745c10ae4110b184e697221f6a08014120f0
unknown <hope_ff@mail.ru>
2022-02-05 11:24:00 +0300

    ffF

--- src/modal/modal.jsx
+++ src/modal/modal.jsx
@@ -1,13 +1,32 @@
 import React from "react";
-import "./modal.css"
+import  "./modal.css";
 
-const Modal = ({active,setActive,children}) => {
-    return(
-        <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
-            <div className={active ? "modal__content active" : "modal__content"} onClick={e => e.stopPropagation()}>
-                {children}
+const Modal = ({visible = false, onClose, children}) => {
+
+    const onKeydown = key => {
+        // eslint-disable-next-line default-case
+        switch (key) {
+            case 'Escape':
+                onClose()
+                break
+        }
+    }
+    React.useEffect(() => {
+        document.addEventListener('keydown', onKeydown)
+        return () => document.removeEventListener('keydown', onKeydown)
+    })
+    if (!visible) return null;
+    return (
+        <div>
+            <div className="modal" onClick={onClose}>
+                <div className="modal-dialog" onClick={e => e.stopPropagation()}>
+                    {children}
+                    <span className="modal-close" onClick={onClose}>
+                 &times;
+             </span>
+                </div>
             </div>
-        </div>
-    )
+        </div>)
 }
+
 export default Modal
